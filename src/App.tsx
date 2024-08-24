cc931a2d07733895b382a4786053858785f430ff
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2024-08-25 01:01:44 +0530

    added seed phrase functionality and using that the user can create multiple wallets in eth and sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { generateMnemonic } from "bip39";
+import { EthWallet } from './component/EthWallet';
+import { SolanaWallet } from './component/SolanaWallet';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [mnemonic, setMnemonic] = useState("");
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <button onClick={async function() {
+        const mn = await generateMnemonic();
+        setMnemonic(mn)
+      }}>
+      Create Seed Phrase
+    </button>
+    <p>{mnemonic}</p>
+    <EthWallet mnemonic={mnemonic} />
+    <SolanaWallet mnemonic={mnemonic} />
     </>
   )
 }
